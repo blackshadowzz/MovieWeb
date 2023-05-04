34c609124c8b626b8f861e27b8aa3be775414ad1
JayKosal <Kosalaction007@gmail.com>
2023-05-05 00:43:36 +0700

    COMII

---
+++ MoviesWebApp/Migrations/AuthDb/20230504070531_MHt.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoviesWebApp.Migrations.AuthDb
+{
+    /// <inheritdoc />
+    public partial class MHt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
